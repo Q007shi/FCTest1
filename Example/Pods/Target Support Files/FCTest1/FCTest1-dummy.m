88e9fcd65c8931010b3868426de1158699c42936
shifucai <2585299617@qq.com>
2020-02-08 22:35:19 +0800

    1

---
+++ Example/Pods/Target Support Files/FCTest1/FCTest1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FCTest1 : NSObject
+@end
+@implementation PodsDummy_FCTest1
+@end
---
+++ FCTest1/Classes/FCTest.swift
@@ -0,0 +1,14 @@
+//
+//  FCTest.swift
+//  FCTest1
+//
+//  Created by 石富才 on 2020/2/8.
+//
+
+import UIKit
+
+class FCTest: NSObject {
+    
+    var name: String?
+
+}
